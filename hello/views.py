757ae40b41cdef1e2603e3105da350e8b4b8abd7
thepinkturtle <devinlax@hotmail.com>
2018-05-01 22:05:01 -0700

    index.html is landing page

--- hello/views.py
+++ hello/views.py
@@ -4,11 +4,19 @@ from django.http import HttpResponse
 from .models import Greeting
 import requests
 import os
-
+#remember to start a new 'pipenv shell'
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
     # return HttpResponse('Hello! ' * times)
     return HttpResponse('Hello from Python!')
 
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'index.html', {'greetings': greetings})
 
