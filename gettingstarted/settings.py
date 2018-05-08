30e5bc07b124790ae4251671b46ad72d341b54b4
thepinkturtle <devinlax@hotmail.com>
2018-05-08 00:42:50 -0700

    configuration for postgre database

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lazylab_db',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
